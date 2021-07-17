d5431cd97d8d4acd1e6f9b5868886f293cb9dc8f
starshine711 <59994288+starshine711@users.noreply.github.com>
2021-07-17 21:31:16 +0800

    gaokao time

--- 1.py
+++ 1.py
@@ -1,12 +1,18 @@
-f = open('1.txt','r', encoding='utf8')
-a = f.read().split('    ')
-b=''
+f = open('1.txt','r')
+a = f.readlines()
+b=[]
+print('ok')
 for i in a:
-	for j in i.split('  '):
-   	 	b+=j+'<br>\n'
+    if i!='\n':
+        b.append(i[3:].replace('\n',''))
+    else:
+        print('ok')
+for c in b:
+    if a =='':
+        b.remove(c)
 print(b)
 f.close()
-c = open('12.txt','w', encoding='utf8')
-c.write(b)
+c = open('123.txt','w', encoding='utf8')
+c.write(str(b))
 c.close()
 input('..')
